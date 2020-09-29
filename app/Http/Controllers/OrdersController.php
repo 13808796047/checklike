771dd13b1b9a0d97b39b4aac47b4bf2d2f72445c
Summer <csy13808796047@gmail.com>
2020-09-29 10:02:11 +0800

    支付配置修改

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -49,15 +49,6 @@ class OrdersController extends Controller
 
     public function show(Order $order)
     {
-//        dd(app(OrderApiHandler::class)->getRequestHeader());
-//        $orderimg = app(OrderimgHandler::class);
-//        return $orderimg->generate();
-//        $disk = Storage::disk('public');
-//        $directory = '/test';
-//        $files = $disk->files($directory);
-//        foreach($files as $file) {
-//            dispatch(new FileWords($file))->delay(now()->addSeconds(2));
-//        }
         // 校验权限
         $this->authorize('own', $order);
         return view('orders.show', compact('order'));
