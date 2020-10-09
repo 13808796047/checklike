92ded9b1def66aecf79a39a7b613b380a503230c
shahao1025 <361719196@qq.com>
2020-10-09 10:34:48 +0800

    test

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -348,21 +348,21 @@
     <div class="modal-content">
       <div style="padding:20px;">
         <div>
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="phone">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="xgpsd">
             密码:
           </label>
           <input
               class="appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="xgphone"
+              name="xgpsd"
               id="xgpsd" type="text" placeholder="请输入密码">
         </div>
         <div>
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="phone">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="xgsurepsd">
             确认密码:
           </label>
           <input
               class="appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="phone"
+              name="xgsurepsd"
               id="xgsurepsd" type="text" placeholder="请输入确认密码">
         </div>
         <div class="flex items-center justify-evenly my-4">
