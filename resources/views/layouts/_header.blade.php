3c6b301f981cebff5c351b7008ce73c15976cd12
shahao1025 <361719196@qq.com>
2020-10-10 13:42:31 +0800

    test

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -412,8 +412,8 @@
             <input
               class="appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               name="xgpsd"
-              id="bindphonenum" type="text" placeholder="验证码">
-            <button type="button" class="btn btn-primary btn-sm">发送验证码</button>
+              id="bindphonenum" type="text" placeholder="验证码" style="width:63%;">
+            <button type="button" class="btn btn-primary" style="margin-left:10px;">发送验证码</button>
           </div>
         </div>
         <div class="flex items-center justify-evenly my-4">
