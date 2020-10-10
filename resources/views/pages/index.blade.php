2fffc7c33bd9ec015b5972aaf31ddf8bf6956fb3
shahao1025 <361719196@qq.com>
2020-10-10 11:04:40 +0800

    test

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -628,7 +628,7 @@
       // });
       // $().UItoTop({easingType: 'easeOutQuart'});
        //模态框打开
-       console.log("xixix",{!!$Auth!!})
+       console.log("xixix",{!!$auth!!})
       var timer = null
       $('#staticBackdrop').on('show.bs.modal', function () {
         axios.get("/official_account").then(res=>{
