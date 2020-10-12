c9b29918efd0743d2ec2d1036d4358640fdb3641
shahao1025 <361719196@qq.com>
2020-10-12 10:12:44 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
   <title>@yield('title', '联文') _维普论文检测系统</title>
 
   <!-- Styles -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   <style>
     .newbody {
@@ -66,6 +67,7 @@
 </div>
 
 <!-- Scripts -->
+<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
 <script src="{{ mix('js/app.js') }}"></script>
 @yield('scripts')
 <script !src="">
