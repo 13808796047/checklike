ce65d5bb11d45a7b9d83696ab12bcf862e97bbd9
shahao1025 <361719196@qq.com>
2020-10-12 10:46:54 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 
   <!-- Styles -->
   <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
-  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css"/>
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   <style>
     .newbody {
