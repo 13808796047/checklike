e7165a0eed433890eb0d49d0f7bcb619c66075c9
shahao1025 <361719196@qq.com>
2020-10-13 17:52:53 +0800

    test

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -444,29 +444,26 @@
         } else {
           $('#tosubmit').css("display", "none");
           $('#submitBtn').css("display", "block");
-          console.log($('#content').val(),"xifdsadfsxi")
-          $('#content').val().replace(/\\n/g,'\\r\\n');
-          console.log($('#content').val(),"xif")
-          // $('#content').val().replace(/\n|\r\n/g,'<br>').replace(/\s/g,'&nbsp;');
-          // console.log("xixi", $('#content').val())
-          // axios.post('{{route('orders.store')}}', {
-          //     cid: $('#cid').val(),
-          //     from: 'pc',
-          //     type: 'content',
-          //     content: $('#content').val(),
-          //     title: $('#title').val(),
-          //     writer: $('#writer').val(),
-          //     endDate: $('#element_id').val()
-          //   }
-          // ).then(res => {
-          //   console.log(res, 3123123)
-          //   var order = res.data.data
-          //   location.href = '/orders/' + res.data.data.id
-          // }).catch(err => {
-          //   alert('提交失败，请重试')
-          //   $('#tosubmit').css("display", "block");
-          //   $('#submitBtn').css("display", "none")
-          // })
+
+          axios.post('{{route('orders.store')}}', {
+              cid: $('#cid').val(),
+              from: 'pc',
+              type: 'content',
+              // content: $('#content').val(),
+              content: $('#content').val().replace(/\\n/g,'\\r\\n'),
+              title: $('#title').val(),
+              writer: $('#writer').val(),
+              endDate: $('#element_id').val()
+            }
+          ).then(res => {
+            console.log(res, 3123123)
+            var order = res.data.data
+            location.href = '/orders/' + res.data.data.id
+          }).catch(err => {
+            alert('提交失败，请重试')
+            $('#tosubmit').css("display", "block");
+            $('#submitBtn').css("display", "none")
+          })
         }
       })
 
