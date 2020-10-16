e55f7b9040ca53f3fdb0945b31310bec40280296
Summer <csy13808796047@gmail.com>
2020-10-16 11:01:49 +0800

    docx字数检查api

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -36,8 +36,7 @@ class OrderService
                     }
                 } else {
                     $res = app(FileWordsHandle::class)->submitCheck($result->path);
-                    dd($res);
-                    $words = app(FileWordsHandle::class)->queryParsing($res['data']['orderid'])['data']['wordCount'];
+                    $words = app(FileWordsHandle::class)->queryParsing($res['data']['orderid']);
                     dd($words);
                     $content = read_docx($result->real_path);
                     if($category->classid == 4) {
