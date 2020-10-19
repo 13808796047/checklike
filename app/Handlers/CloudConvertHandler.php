cf7663310ef9fb023be4afed12911ec7551bb1ad
Summer <csy13808796047@gmail.com>
2020-10-19 09:36:01 +0800

    docx字数检查api

--- app/Handlers/CloudConvertHandler.php
+++ app/Handlers/CloudConvertHandler.php
@@ -4,7 +4,28 @@
 namespace App\Handlers;
 
 
+use CloudConvert\Models\Job;
+
 class CloudConvertHandler
 {
+    public function Convert()
+    {
+        $job = (new Job())
+            ->addTask(new Task('import/upload', 'upload-my-file'))
+            ->addTask(
+                (new Task('convert', 'convert-my-file'))
+                    ->set('input', 'import-my-file')
+                    ->set('output_format', 'pdf')
+            )
+            ->addTask(
+                (new Task('export/url', 'export-my-file'))
+                    ->set('input', 'convert-my-file')
+            );
 
+        $cloudconvert->jobs()->create($job);
+
+        $uploadTask = $job->getTasks()->whereName('upload-my-file')[0];
+
+        $cloudconvert->tasks()->upload($uploadTask, fopen('./file.pdf', 'r'));
+    }
 }
