2ea2c532cd511eca7fa2c4f8e839fdd721647daf
Summer <csy13808796047@gmail.com>
2020-10-20 17:52:50 +0800

    docx字数检查api

--- app/Http/Controllers/AutoCheckController.php
+++ app/Http/Controllers/AutoCheckController.php
@@ -31,7 +31,7 @@ class AutoCheckController extends Controller
         $result = app(AiWriterHandler::class)->getContent($request->input('content', ''));
 
         $user->decreaseJcTimes();
-        return $result;
+        return $result['data'];
 
 //        $data = AutoCheck::create([
 //            'content_before' => $request->input('content'),
