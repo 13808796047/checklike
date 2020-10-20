6b608d1b7ca3478c6611f7d6d848ee5abd308bfc
Summer <csy13808796047@gmail.com>
2020-10-20 15:13:13 +0800

    docx字数检查api

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -37,7 +37,7 @@ class OrderService
                     $words = app(FileWordsHandle::class)->queryParsing($res['data']['orderid'])['data']['wordCount'];
                     if($category->classid == 4 && $file->type == 'docx') {
                         $content = read_docx($file->real_path);
-//                        $result = app(FileUploadHandler::class)->saveTxt($content, 'files', $user->id);
+                        $result = app(FileUploadHandler::class)->saveTxt($content, 'files', $user->id);
                     }
                 }
             } else {
@@ -110,7 +110,6 @@ class OrderService
 
     protected function checkWords(Order $order)
     {
-        dd($order);
         if($order->category->classid == 4) {
             if($order->file->type == 'docx') {
                 $content = read_docx($order->file->real_path);
