5610df85370abd5d36d02d9680a5c8fead5328cd
Summer <csy13808796047@gmail.com>
2020-10-20 17:09:50 +0800

    docx字数检查api

--- app/Handlers/AiWriterHandler.php
+++ app/Handlers/AiWriterHandler.php
@@ -4,28 +4,33 @@
 namespace App\Handlers;
 
 
-use GuzzleHttp\Client;
-
 class AiWriterHandler
 {
-    protected $api;
-    protected $token;
-
-    public function __construct(Client $client)
-    {
-        $this->http = $client;
-        $this->api = 'http://apis.5118.com/wyc/akey';
-//        $this->token = '$2y$10$K8gSIdvi2tpyJ94OZ/iKUOIFWZj2aKhjxTYddndl8tMNb5mvjr60G';
-    }
-
     public function getContent($content)
     {
-        $array = [
-            'form_params' => [
-                'content' => $content
-            ]
-        ];
-        $response = $this->http->request("POST", $this->api, $array);
-        return json_decode($response->getBody(), true);
+        $host = "http://apis.5118.com";
+        $path = "/wyc/akey";
+        $method = "POST";
+        $apikey = "20FED99DBE644B818379C20653A8806F";
+        $headers = [];
+        array_push($headers, "Authorization:" . $apikey);
+        //根据API的要求，定义相对应的Content-Type
+        array_push($headers, "Content-Type" . ":" . "application/x-www-form-urlencoded; charset=UTF-8");
+        $querys = "";
+        $bodys = "txt=txt&th=th&filter=filter&corewordfilter=corewordfilter";
+        $url = $host . $path;
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_FAILONERROR, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        if(1 == strpos("$" . $host, "https://")) {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        }
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $bodys);
+        return curl_exec($curl);
     }
 }
