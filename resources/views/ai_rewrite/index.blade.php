f2771d2b82224f1dd3ffb5c411a4cdec2d17a80b
shahao1025 <361719196@qq.com>
2020-10-21 15:52:24 +0800

    test

--- resources/views/ai_rewrite/index.blade.php
+++ resources/views/ai_rewrite/index.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.app')
 @section('title','智能查重')
 @section('styles')
+<link href="{{asset('asset/css/toast-min.css')}}" rel="stylesheet" />
 <link rel="stylesheet" href="{{asset('asset/css/check.css')}}">
-  <style>
+<style>
+  del { background: #FF4040; }
+  ins { background: #00ff21;text-decoration:none; }
 </style>
   <!-- <link href="https://css.lianwen.com/css/public_c.css?v=2018v1" type="text/css" rel="stylesheet"/>
   <link href="https://css.lianwen.com/css/index_2017.css" type="text/css" rel="stylesheet"/> -->
@@ -119,7 +122,6 @@
 </div>
 @stop
 @section('scripts')
-
 <script type="text/javascript" src="{{ asset('asset/js/qrcode.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('asset/js/copy_cliboard.js') }}"></script>
 <script type="text/javascript" src="{{ asset('asset/js/diff.js') }}"></script>
@@ -160,7 +162,7 @@
     $('#exampleModal').modal('hide')
     $('#beingModal').modal('show')
     let num = $("#requestcishuNum").html();
-    console.log(num,"xixi")
+
   })
 
   </script>
