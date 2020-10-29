9e0f5235aa1b30528d880191ca5005db06840ecf
Summer <csy13808796047@gmail.com>
2020-10-29 11:03:11 +0800

    支付转换doc

--- app/Handlers/FileWordsHandle.php
+++ app/Handlers/FileWordsHandle.php
@@ -76,7 +76,7 @@ class FileWordsHandle
         try {
             do {
                 $response = $this->http->post($this->uri . '/query-parsing', $query);
-                dd($response);
+                dd($response->getStatusCode());
             } while (!$response);
             return json_decode($response->getbody()->getContents(), true);
         } catch (\Exception $e) {
