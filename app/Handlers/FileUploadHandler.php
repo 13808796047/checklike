22ee64c69bdbc21e5b17a4e90c29dcef75176d91
Summer <csy13808796047@gmail.com>
2020-10-29 14:21:44 +0800

    支付转换doc

--- app/Handlers/FileUploadHandler.php
+++ app/Handlers/FileUploadHandler.php
@@ -37,7 +37,7 @@ class FileUploadHandler
             if(!file_exists($upload_path)) {
                 mkdir($upload_path, 0777, true);
             }
-            chmod($upload_path, 0777);
+//            chmod($upload_path, 0777);
             file_put_contents($upload_path . '/' . $filename, $txt_content);
             return [
                 'path' => config('app.url') . "/$folder_name/$filename"
