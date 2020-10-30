dd862cfac28b562bf2bde4a1fe98a6b1f15e107e
Summer <csy13808796047@gmail.com>
2020-10-30 14:52:28 +0800

    支付转换doc

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
 
     public function showLoginForm()
     {
-        return view('domained::pages.index');
+        return view('pages.index');
     }
 
     public function username()
