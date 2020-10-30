b076d4c0a1e2c637c70c56be3c05cf87d201388f
shahao1025 <361719196@qq.com>
2020-10-30 15:10:07 +0800

    test

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <div id="lwfooter" class="w-full flex  justify-center bg-gray-800" style="height:60px;bottom:0;">
   <div class="p-3 text-white">
-    COPYRIGHT © 2005-2020 深圳智汇教育有限公司 粤ICP备16093775号 学信检测 版权所有 客服热线：400-823-8869
+     Copyright © 2010-2022.昆明云众网络科技有限公司 All rights reserved. 滇ICP备13004902号
   </div>
 </div>
