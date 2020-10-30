c596f11aa052c0b9c1c027c9fb8f08c9d78a6cf5
Summer <csy13808796047@gmail.com>
2020-10-30 11:22:46 +0800

    支付转换doc

--- app/Handlers/FileUploadHandler.php
+++ app/Handlers/FileUploadHandler.php
@@ -36,7 +36,7 @@ class FileUploadHandler
         $upload_path = public_path() . '/' . $folder_name;
         $filename = $file_prefix . '_' . time() . '_' . \Str::random(10) . '.txt';
         try {
-            if(!file_exists($upload_path)) {
+            if(!is_dir($upload_path)) {
                 mkdir($upload_path, 0777, true);
                 chmod($upload_path, 0777);
             }
