73d01f7093b0722dfc6b7f30135812691df7a19c
shahao1025 <361719196@qq.com>
2020-11-02 11:16:07 +0800

    test

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app')
 @section('title', '学信检测')
 @section('styles')
-  <link rel="stylesheet" href="{{asset('asset/css/bootstrap4.css')}}">
-  <link href="{{asset('asset/css/styles.css')}}" rel="stylesheet"/>
+<link rel="stylesheet" href="{{asset('asset/css/bootstrap4.css')}}">
   <link rel="stylesheet" href="{{asset('asset/css/ionicons.min.css')}}">
   <link rel="stylesheet" href="{{asset('asset/css/slick.css')}}">
   <link rel="stylesheet" href="{{asset('asset/css/slick-theme.css')}}">
   <link rel="stylesheet" href="{{asset('asset/css/jquery.fancybox.css')}}">
   <link rel="stylesheet" href="{{asset('asset/css/animate.min.css')}}">
-
+  <link href="{{asset('asset/css/styles.css')}}" rel="stylesheet"/>
   <style>
     .swal-modal {
       width: 350px;
@@ -679,7 +678,7 @@
 <script type="text/javascript" src="{{asset('asset/newjs/script.js')}}"></script>
 <script type="text/javascript" src="{{asset('asset/newjs/jquery-migrate-1.2.1.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('asset/newjs/bootstrap.min.js')}}"></script>
-<!-- <script type="text/javascript" src="{{asset('asset/newjs/jquery.easing.min.js')}}"></script> -->
+<script type="text/javascript" src="{{asset('asset/newjs/jquery.easing.min.js')}}"></script>
 <!-- <script type="text/javascript" src="js/smoothscroll.js"></script>
 <script type="text/javascript" src="js/response.min.js"></script> -->
 <script type="text/javascript" src="{{asset('asset/newjs/jquery.placeholder.min.js')}}"></script>
