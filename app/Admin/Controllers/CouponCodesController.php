e9a0e8e645f591fb7865ae5bfab08fe9cb07f3ab
Summer <csy13808796047@gmail.com>
2020-11-05 14:04:22 +0800

    优惠券功能

---
+++ app/Admin/Controllers/CouponCodesController.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Admin\Controllers;
+
+
+use App\Admin\Repositories\CouponCode;
+use Dcat\Admin\Controllers\AdminController;
+use Dcat\Admin\Grid;
+
+class CouponCodesController extends AdminController
+{
+    public function grid()
+    {
+        return Grid::make(new CouponCode(['user', 'category']), function(Grid $grid) {
+            // 默认按创建时间倒序排序
+            $grid->model()->orderBy('created_at', 'desc');
+            // 第一列显示id字段，并将这一列设置为可排序列
+            $grid->column('id', 'ID')->sortable();
+            $grid->code('卡号');
+            $grid->type('类型')->display(function($value) {
+                return \App\Models\CouponCode::$typeMap[$value];
+            });
+            // 根据不同的折扣类型用对应的方式来展示
+            $grid->value('折扣')->display(function($value) {
+                return $this->type === \App\Models\CouponCode::TYPE_FIXED ? '$' . $value : $value . '%';
+            });
+            $grid->min_amount('生效金额');
+            $grid->enable_days('有效天数');
+            $grid->status('状态')->display(function($value) {
+                return \App\Models\CouponCode::$statusMap[$value];
+            });
+            $grid->column('user.phone', '会员账号');
+            $grid->column('category.name', '生效系统');
+            $grid->unenable_date('失效时间');
+        });
+    }
+}
---
+++ database/migrations/2020_11_04_151050_create_coupon_codes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupon_codes', function(Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique()->comment('卡号');
+            $table->string('type')->comment('类型');
+            $table->decimal('value')->nullable()->comment('折扣值');
+            $table->unsignedBigInteger('cid')->comment('生效系统')->nullable();
+            $table->foreign('cid')->references('id')->on('categories')->onDelete('set null');
+            $table->unsignedBigInteger('uid')->comment('会员id')->nullable();
+            $table->foreign('uid')->references('id')->on('users')->onDelete('set null');
+            $table->decimal('min_amount')->nullable()->comment('生效金额');
+            $table->integer('enable_days')->comment('有效天数');
+            $table->datetime('unabled_date')->nullable()->comment('失效日期');
+            $table->string('status')->comment('状态');
+            $table->string('remark')->nullable()->comment('备注');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupon_codes');
+    }
+}
