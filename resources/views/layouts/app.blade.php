d140e3f65f15e30a590ff3d0d2451eec241e2189
shahao1025 <361719196@qq.com>
2020-11-06 11:20:02 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
   <link href="{{asset('asset/css/styles.css')}}" rel="stylesheet"/>
   <link rel="stylesheet" href="{{asset('asset/css/jquery-confirm.css')}}">
+  <link rel="stylesheet" href="{{asset('asset/css/bootstrap4.css')}}">
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
   <style>
