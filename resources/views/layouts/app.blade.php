2aaea056bf84fa8d07e536d3913c31478affa86f
shahao1025 <361719196@qq.com>
2020-11-06 09:48:23 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,7 +79,7 @@
 
 <!-- Scripts -->
 <!-- <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script> -->
-<script src="{{asset('asset/css/jquery-confirm.js'}}"></script>
+<script src="{{asset('asset/js/jquery-confirm.js'}}"></script>
 <script src="{{ mix('js/app.js') }}"></script>
 @yield('scripts')
 <script !src="">
