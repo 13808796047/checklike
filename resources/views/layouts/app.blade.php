638c676a4ec89595f9e7f02628dac2605f17a5eb
shahao1025 <361719196@qq.com>
2020-11-06 10:13:45 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
 
 <!-- Scripts -->
 <!-- <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script> -->
-<!-- <script type="text/javascript" src="{{asset('asset/newjs/jquery-1.11.3.min.js')}}"></script> -->
+<script type="text/javascript" src="{{asset('asset/newjs/jquery-1.11.3.min.js')}}"></script>
 
 <script src="{{ mix('js/app.js') }}"></script>
 <!-- <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.js"></script> -->
