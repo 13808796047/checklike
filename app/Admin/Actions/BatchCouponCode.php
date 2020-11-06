93f79c9b955981c0f4d228fdfb918d572dae9b6f
Summer <csy13808796047@gmail.com>
2020-11-06 10:54:22 +0800

    后台优惠卡

---
+++ app/Admin/Actions/BatchCouponCode.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Admin\Actions;
+
+use Dcat\Admin\Actions\Action;
+use Dcat\Admin\Actions\Response;
+use Dcat\Admin\Traits\HasPermissions;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class BatchCouponCode extends Action
+{
+    /**
+     * @return string
+     */
+    protected $title = '批量生成卡';
+
+    /**
+     * Handle the action request.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function handle(Request $request)
+    {
+        // dump($this->getKey());
+
+        return $this->response()->success('Processed successfully.')->redirect('/');
+    }
+
+    /**
+     * @return string|array|void
+     */
+    public function confirm()
+    {
+        // return ['Confirm?', 'contents'];
+    }
+
+    /**
+     * @param Model|Authenticatable|HasPermissions|null $user
+     *
+     * @return bool
+     */
+    protected function authorize($user): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    protected function parameters()
+    {
+        return [];
+    }
+}
