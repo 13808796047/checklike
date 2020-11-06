43558850d4a28262b22710e6b3c1d802b82d57ef
shahao1025 <361719196@qq.com>
2020-11-06 15:11:40 +0800

    test

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -668,7 +668,7 @@
   <script type="text/javascript" src=" {{ asset('asset/js/easing.js') }}"></script> -->
   {{--  <script src="{{ asset('asset/js/responsiveslides.min.js') }}"></script>--}}
   <!-- JavaScripts -->
-
+<!-- <script type="text/javascript" src="{{asset('asset/newjs/jquery-1.11.3.min.js')}}"></script> -->
 <script type="text/javascript" src="{{asset('asset/newjs/jquery-migrate-1.2.1.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('asset/js/jquery-confirm.js')}}" ></script>
 <script type="text/javascript" src="{{asset('asset/newjs/script.js')}}"></script>
@@ -836,7 +836,7 @@
       //忘记密码
       $("#forgetpsw").click(function(){
         console.log("xixo")
-        $("#staticBackdrop").modal("hide")
+        $("#staticBackdrop").css('display:none')
         // $("#staticBackdrop").remove()
         // $("#staticBackdrop").removeClass();
         // console.log("xixi")
