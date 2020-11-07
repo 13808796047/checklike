b5826de99949928ec384327d2471e7c8088cc56e
Summer <csy13808796047@gmail.com>
2020-11-07 16:11:21 +0800

    后台

--- app/Admin/Forms/CreateCouponCode.php
+++ app/Admin/Forms/CreateCouponCode.php
@@ -44,32 +44,33 @@ class CreateCouponCode extends Form
 //            $this->text('text2');
 //
 //        });
-        $this->radio('radio', '')
-            ->when(1, function(Form $form) {
-                $form->number('enable_days', '有效天数');
-                $form->datetime('unenable_date', '失效日期');
-                $form->number('num', '生成数量');
-                $form->textarea('remark', '备注');
-            })
-            ->when(2, function(Form $form) {
-                $form->number('min_amount', '满')->required();
-                $form->number('value', '减');
-//                $form->select('cid', '生效系统')->options('/category_options');
-                $form->number('enable_days', '有效天数');
-                $form->datetime('unenable_date', '失效日期');
-                $form->number('num', '生成数量');
-                $form->textarea('remark', '备注');
-            })
-            ->when(3, function(Form $form) {
+        $this->select('value')->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
+//        $this->radio('radio', '')
+//            ->when(1, function(Form $form) {
+//                $form->number('enable_days', '有效天数');
+//                $form->datetime('unenable_date', '失效日期');
+//                $form->number('num', '生成数量');
+//                $form->textarea('remark', '备注');
+//            })
+//            ->when(2, function(Form $form) {
+//                $form->number('min_amount', '满')->required();
+//                $form->number('value', '减');
+////                $form->select('cid', '生效系统')->options('/category_options');
+//                $form->number('enable_days', '有效天数');
+//                $form->datetime('unenable_date', '失效日期');
+//                $form->number('num', '生成数量');
+//                $form->textarea('remark', '备注');
+//            })
+//            ->when(3, function(Form $form) {
 //                $form->select('value')->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
-                $form->number('enable_days', '有效天数');
-                $form->datetime('unenable_date', '失效日期');
-                $form->number('num', '生成数量');
-                $form->textarea('remark', '备注');
-//                $form->select('cid', '生效系统')->options('/category_options');
-            })
-            ->options($this->options)
-            ->default(1);
+//                $form->number('enable_days', '有效天数');
+//                $form->datetime('unenable_date', '失效日期');
+//                $form->number('num', '生成数量');
+//                $form->textarea('remark', '备注');
+////                $form->select('cid', '生效系统')->options('/category_options');
+//            })
+//            ->options($this->options)
+//            ->default(1);
     }
 
     /**
