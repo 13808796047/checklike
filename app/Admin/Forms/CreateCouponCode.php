e2dd0e467525da20d4b42b274748b0316c824e45
Summer <csy13808796047@gmail.com>
2020-11-07 15:52:24 +0800

    后台

--- app/Admin/Forms/CreateCouponCode.php
+++ app/Admin/Forms/CreateCouponCode.php
@@ -60,7 +60,7 @@ class CreateCouponCode extends Form
                 $form->textarea('remark', '备注');
             })
             ->when(3, function(Form $form) {
-                $form->select('value', '卡密折扣')->options($this->options);
+                $form->select('value', '卡密折扣')->options($this->options)->default(1);
 
                 $form->number('enable_days', '有效天数');
                 $form->datetime('unenable_date', '失效日期');
