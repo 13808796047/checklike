16d7769afd0fba24e19eba1a2682aa435c0adc16
Summer <csy13808796047@gmail.com>
2020-11-07 17:11:30 +0800

    后台

--- app/Admin/Forms/CreateCouponCode.php
+++ app/Admin/Forms/CreateCouponCode.php
@@ -29,60 +29,60 @@ class CreateCouponCode extends Form
      */
     public function form()
     {
-        $this->tab('VIP卡', function() {
-            $this->select('value', 'xxx')->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
-            $this->number('enable_days', '有效天数');
-            $this->datetime('unenable_date', '失效日期');
-            $this->number('num', '生成数量');
-            $this->textarea('remark', '备注');
-        });
-
-        $this->tab('满减卡', function() {
-            $this->number('min_amount', '满')->required();
-            $this->number('value1', '减');
-            $this->select('cid', '生效系统')->options(Model::class)->ajax('/category_options');
-            $this->number('enable_days', '有效天数');
-            $this->datetime('unenable_date', '失效日期');
-            $this->number('num', '生成数量');
-            $this->textarea('remark', '备注');
-
-        });
-        $this->tab('折扣卡', function() {
-//            $this->select('value')->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
-            $this->number('enable_days', '有效天数');
-            $this->datetime('unenable_date', '失效日期');
-            $this->number('num', '生成数量');
-            $this->textarea('remark', '备注');
-            $this->select('cid', '生效系统')->options(Model::class)->ajax('/category_options');
-
-        });
+//        $this->tab('VIP卡', function() {
+//            $this->select('value', 'xxx')->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
+//            $this->number('enable_days', '有效天数');
+//            $this->datetime('unenable_date', '失效日期');
+//            $this->number('num', '生成数量');
+//            $this->textarea('remark', '备注');
+//        });
+//
+//        $this->tab('满减卡', function() {
+//            $this->number('min_amount', '满')->required();
+//            $this->number('value1', '减');
+//            $this->select('cid', '生效系统')->options(Model::class)->ajax('/category_options');
+//            $this->number('enable_days', '有效天数');
+//            $this->datetime('unenable_date', '失效日期');
+//            $this->number('num', '生成数量');
+//            $this->textarea('remark', '备注');
+//
+//        });
+//        $this->tab('折扣卡', function() {
+////            $this->select('value')->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
+//            $this->number('enable_days', '有效天数');
+//            $this->datetime('unenable_date', '失效日期');
+//            $this->number('num', '生成数量');
+//            $this->textarea('remark', '备注');
+//            $this->select('cid', '生效系统')->options(Model::class)->ajax('/category_options');
+//
+//        });
 //        $this->select('value')->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
-//        $this->radio('radio', '')
-//            ->when(1, function() {
-//                $this->number('enable_days', '有效天数');
-//                $this->datetime('unenable_date', '失效日期');
-//                $this->number('num', '生成数量');
-//                $this->textarea('remark', '备注');
-//            })
-//            ->when(2, function() {
-//                $this->number('min_amount', '满')->required();
-//                $this->number('value', '减');
-////                $this->select('cid', '生效系统')->options('/category_options');
-//                $this->number('enable_days', '有效天数');
-//                $this->datetime('unenable_date', '失效日期');
-//                $this->number('num', '生成数量');
-//                $this->textarea('remark', '备注');
-//            })
-//            ->when(3, function() {
-//                $this->select('value')->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
-//                $this->number('enable_days', '有效天数');
-//                $this->datetime('unenable_date', '失效日期');
-//                $this->number('num', '生成数量');
-//                $this->textarea('remark', '备注');
-////                $form->select('cid', '生效系统')->options('/category_options');
-//            })
-//            ->options($this->options)
-//            ->default(1);
+        $this->radio('radio', '')
+            ->when(1, function() {
+                $this->number('enable_days', '有效天数');
+                $this->datetime('unenable_date', '失效日期');
+                $this->number('num', '生成数量');
+                $this->textarea('remark', '备注');
+            })
+            ->when(2, function() {
+                $this->number('min_amount', '满')->required();
+                $this->number('value1', '减');
+//                $this->select('cid', '生效系统')->options('/category_options');
+                $this->number('enable_days', '有效天数');
+                $this->datetime('unenable_date', '失效日期');
+                $this->number('num', '生成数量');
+                $this->textarea('remark', '备注');
+            })
+            ->when(3, function() {
+                $this->select('value')->options([1 => 'foo', 2 => 'bar', 'val' => 'Option name']);
+                $this->number('enable_days', '有效天数');
+                $this->datetime('unenable_date', '失效日期');
+                $this->number('num', '生成数量');
+                $this->textarea('remark', '备注');
+//                $form->select('cid', '生效系统')->options('/category_options');
+            })
+            ->options($this->options)
+            ->default(1);
     }
 
     /**
