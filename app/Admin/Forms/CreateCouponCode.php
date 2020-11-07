61404ab06c629ae432ea798345f67b39b5fc9988
Summer <csy13808796047@gmail.com>
2020-11-07 09:36:33 +0800

    支付转换doc

--- app/Admin/Forms/CreateCouponCode.php
+++ app/Admin/Forms/CreateCouponCode.php
@@ -50,10 +50,8 @@ class CreateCouponCode extends Form
             $form->textarea('remark', '备注');
         })
             ->when(2, function(Form $form) {
-                $form->row(function(Row $form) {
-                    $form->width(4)->number('min_amount', '满')->required();
-                    $form->width(3)->number('value', '减');
-                });
+                $form->width(4)->number('min_amount', '满')->required();
+                $form->width(3)->number('value', '减');
             })
             ->when(3, function(Form $form) {
                 $form->image('image');
