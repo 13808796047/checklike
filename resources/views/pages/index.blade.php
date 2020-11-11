f4dc2d7a6ef4bae71aa346b0c693f9c2528ce0a0
shahao1025 <361719196@qq.com>
2020-11-11 14:29:31 +0800

    test

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -710,11 +710,14 @@
       // });
       // $().UItoTop({easingType: 'easeOutQuart'});
        //模态框打开
-       if($('#navigation').hasClass('affix')){
+       $(window).scoll(()=>{
+        if($('#navigation').hasClass('affix')){
          console.log("含有")
-       }else{
+        }else{
          console.log("不含有")
-       }
+        }
+       })
+
       var isBindPhone = {!!Auth::user()!!}
       console.log(isBindPhone,"那哈哈")
       if(isBindPhone && isBindPhone.phone){
