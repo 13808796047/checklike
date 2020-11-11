9f015eb0231f155317b3c8fc8fa3d771aa68a203
shahao1025 <361719196@qq.com>
2020-11-11 11:44:07 +0800

    test

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -173,7 +173,7 @@
         </div>
         <a type="button" id="bottonsubmit" style="height:33px;margin:13px auto" href="javascript:;"
 						 class="btn btn-primary btn-sm sbtn">提交</a>
-				<a type="button" id="btn-wechat" style="height:33px;margin:13px auto" href="javascript:;"
+				<a type="button" id="btn-wechat" style="height:33px;margin:13px auto;display:none;" href="javascript:;"
 						class="btn btn-primary btn-sm sbtn">提交</a>
 	</div>
 	<div class="rbox fr">
