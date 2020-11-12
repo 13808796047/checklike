c26d7d03afaee83ec7894ec8d66baf0999a5a1fa
shahao1025 <361719196@qq.com>
2020-11-12 17:25:04 +0800

    test

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -27,7 +27,7 @@
 
   <div class="main clearfix" style="flex:1">
 
-    <div class="card topic-reply mt-4" style="margin:30px 60px;width:100%;min-height:calc(100vh * 0.83)">
+    <div class="card topic-reply mt-4" style="margin:30px 60px;width:100%;min-height:calc(100vh * 0.81)">
       <div class="usertitle">基本信息</div>
       <div style="margin:0 18px;">
         <p>用户名：{{Auth::user()->nick_name? Auth::user()->nick_name : Auth::user()->phone }}
