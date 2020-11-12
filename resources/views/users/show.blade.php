58e86ae0af89ec1f2b716ba4cf937742c6ceb482
shahao1025 <361719196@qq.com>
2020-11-12 10:26:27 +0800

    test

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -16,13 +16,15 @@
 <div class="main clearfix" style="flex:1">
 
      <div class="card topic-reply mt-4" style="margin:30px 60px;">
+          <div>基本信息</div>
           <div>
               <p>用户名：{{Auth::user()->phone}}</p>
               <p>手机号：{{Auth::user()->phone}}</p>
               <p>自动降重次数: {{Auth::user()->jc_times}}</p>
               <p>会员:{{Auth::user()->is_free}}</p>
           </div>
-
+          <div>卡券管理</div>
+          <div></div>
           <div class="card-body">
             @include('users._coupon_codes', ['coupon_codes' => $user->couponCodes()->paginate(5)])
           </div>
