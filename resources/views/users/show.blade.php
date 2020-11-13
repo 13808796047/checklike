d3c808c40b58b13e002bd0d3d8c8b7c10efb84ff
shahao1025 <361719196@qq.com>
2020-11-13 10:04:03 +0800

    test

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -27,7 +27,7 @@
 
   <div class="main clearfix" style="flex:1">
 
-    <div class="card topic-reply mt-4" style="margin:30px 60px;width:100%;min-height:calc(100vh * 0.81);max-width:1200px;">
+    <div class="card topic-reply mt-4" style="margin:30px auto;width:100%;min-height:calc(100vh * 0.81);max-width:1200px;">
       <div class="usertitle">基本信息</div>
       <div style="margin:0 18px;">
         <img src="{{Auth::user()->avatar}}" alt="">
