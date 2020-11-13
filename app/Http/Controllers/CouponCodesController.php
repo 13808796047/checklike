825c889cdb034283587a2a2e51ad555cd344306f
Summer <csy13808796047@gmail.com>
2020-11-13 15:44:23 +0800

    卡券

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -24,6 +24,7 @@ class CouponCodesController extends Controller
             ->where('status', CouponCode::STATUS_ACTIVED)
 //            ->where(Carbon::parse('actived_at')->addDays('enable_days')->lt(Carbon::now()))
             ->get();
+        return $coupon_codes;
         return response()->json([
             'data' => $coupon_codes
         ]);
