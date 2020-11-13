bf68d21ca7ba5441af324e5b4291ee42d873912c
shahao1025 <361719196@qq.com>
2020-11-13 10:23:59 +0800

    test

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -29,8 +29,11 @@
 
     <div class="card topic-reply mt-4" style="margin:30px auto;width:100%;min-height:calc(100vh * 0.81);max-width:1200px;">
       <div class="usertitle">基本信息</div>
-      <div style="margin:0 18px;">
+      <div style="margin:0 18px;display:flex;">
+      <div>
         <img src="{{Auth::user()->avatar ? Auth::user()->avatar : asset('asset/images/avtarno.jpg')}}" alt="" style="width:130px;height:130px;border-radius: 50%;">
+      </div>
+      <div>
         <p>用户名：{{Auth::user()->nick_name? Auth::user()->nick_name : Auth::user()->phone }}
         @if(Auth::user()->phone)
         <span class="userword" id="userxiugaipsd">修改密码</span>
@@ -48,6 +51,7 @@
             <span>会员还剩余{{Auth::user()->vip_days}}天</span>
           @endif
         </div>
+      </div>
 
         <p></p>
       </div>
