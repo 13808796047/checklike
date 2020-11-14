b012f6208be9e99f6cfc1a73e83a63062615f715
shahao1025 <361719196@qq.com>
2020-11-14 17:46:30 +0800

    test

--- resources/views/users/_coupon_codes.blade.php
+++ resources/views/users/_coupon_codes.blade.php
@@ -18,6 +18,24 @@
     radial-gradient(10px circle at 10px 5px, transparent 5px, #74D2D4 5px);
     background-size: 15px 5px;
   }
+  .gqcard{
+    width: 220px;
+    position: relative;
+    color: #D1D1D1;
+    background: radial-gradient(circle at 110px 0, transparent 10px, #D1D1D1 0) top left/220px 100% no-repeat;
+    margin: 10px 15px;
+  }
+  .yhcard::after {
+    content: '';
+    position: absolute;
+    height: 5px;
+    width: 100%;
+    left: 0;
+    bottom: -5px;
+    background-image: linear-gradient(to right, #D1D1D1 5px, transparent 5px, transparent),
+    radial-gradient(10px circle at 10px 5px, transparent 5px, #D1D1D1 5px);
+    background-size: 15px 5px;
+  }
 
   .cardpline {
     font-size: 11px;
@@ -34,30 +52,7 @@
   <div style="display: flex;flex-wrap: wrap;">
     @foreach($coupon_codes as $item)
         @if ($item->is_enable == true)
-
           @continue
-        @else
-        <div class="yhcard">
-        <div style="text-align: center;padding-top: 23px;font-size: 29px;font-weight: bold;">
-          @if($item->type =="fixed")
-            ￥{{$item->value}}
-          @else
-            {{$item->value}}折
-          @endif
-        </div>
-        <div style="text-align:center;font-size:11px;">
-          @if($item->type =="vip")
-            VIP专属
-          @else
-            满{{$item->min_amount}}元可用，限一次
-          @endif
-        </div>
-        <div class="cardpline">
-          <p>适用系统：{{$item->category? $item->category->name:"不限"}}</p>
-          <p>有效期：剩余{{$item->enable_days}}天</p>
-          <p>卡券编号：{{$item->code}}</p>
-        </div>
-      </div>
         @endif
       <div class="yhcard">
         <div style="text-align: center;padding-top: 23px;font-size: 29px;font-weight: bold;">
@@ -82,7 +77,35 @@
       </div>
     @endforeach
   </div>
-
+  <div class="usertitle">过期卡券</div>
+  <div style="display: flex;flex-wrap: wrap;">
+    @foreach($coupon_codes as $item)
+        @if ($item->is_enable == false)
+          @continue
+        @endif
+      <div class="gqcard">
+        <div style="text-align: center;padding-top: 23px;font-size: 29px;font-weight: bold;">
+          @if($item->type =="fixed")
+            ￥{{$item->value}}
+          @else
+            {{$item->value}}折
+          @endif
+        </div>
+        <div style="text-align:center;font-size:11px;">
+          @if($item->type =="vip")
+            VIP专属
+          @else
+            满{{$item->min_amount}}元可用，限一次
+          @endif
+        </div>
+        <div class="cardpline">
+          <p>适用系统：{{$item->category? $item->category->name:"不限"}}</p>
+          <p></p>
+          <p>卡券编号：{{$item->code}}</p>
+        </div>
+      </div>
+    @endforeach
+  </div>
 </div>
 <script>
 let fsdafasdf=  {!!$coupon_codes!!}
