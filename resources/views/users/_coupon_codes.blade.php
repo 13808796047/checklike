b2593a6cd1f5c71cf842e02b1e56fee2119c617a
shahao1025 <361719196@qq.com>
2020-11-14 17:50:10 +0800

    test

--- resources/views/users/_coupon_codes.blade.php
+++ resources/views/users/_coupon_codes.blade.php
@@ -18,24 +18,6 @@
     radial-gradient(10px circle at 10px 5px, transparent 5px, #74D2D4 5px);
     background-size: 15px 5px;
   }
-  .gqcard{
-    width: 220px;
-    position: relative;
-    color: #FFFAFA;
-    background: radial-gradient(circle at 110px 0, transparent 10px, #D1D1D1 0) top left/220px 100% no-repeat;
-    margin: 10px 15px;
-  }
-  .yhcard::after {
-    content: '';
-    position: absolute;
-    height: 5px;
-    width: 100%;
-    left: 0;
-    bottom: -5px;
-    background-image: linear-gradient(to right, #D1D1D1 5px, transparent 5px, transparent),
-    radial-gradient(10px circle at 10px 5px, transparent 5px, #D1D1D1 5px);
-    background-size: 15px 5px;
-  }
 
   .cardpline {
     font-size: 11px;
@@ -83,7 +65,7 @@
         @if ($item->is_enable == false)
           @continue
         @endif
-      <div class="gqcard">
+      <div class="yhcard">
         <div style="text-align: center;padding-top: 23px;font-size: 29px;font-weight: bold;">
           @if($item->type =="fixed")
             ￥{{$item->value}}
