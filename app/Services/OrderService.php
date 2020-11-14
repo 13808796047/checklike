475cccd0ab097ecfff611a5bea9d07e58c6008f3
Summer <csy13808796047@gmail.com>
2020-11-14 14:58:07 +0800

    resource

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -117,7 +117,7 @@ class OrderService
                 dispatch(new OrderPendingMsg($order))->delay(now()->addMinutes(2));
             }
             // 开启关闭订单
-            $this->dispatch(new CloseOrder($order), 30);
+            dispatch(new CloseOrder($order), 30);
             return $order;
         });
         return $order;
