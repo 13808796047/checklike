1d2e4eb93d3d8f9e7fe5dbdbb7ff4f911bebb8ef
shahao1025 <361719196@qq.com>
2020-11-14 15:36:21 +0800

    test

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -235,6 +235,7 @@
           let CardBox = $(".cardToast").children()
           CardBox.click(function(){
             $(this).addClass('currentBoder')
+            console.log($(this).siblings())
           })
        }
 
