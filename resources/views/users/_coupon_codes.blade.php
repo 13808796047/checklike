d3bb76a5a9b7f15a96617d2eafba08e0f265f5f0
shahao1025 <361719196@qq.com>
2020-11-16 10:24:10 +0800

    test

--- resources/views/users/_coupon_codes.blade.php
+++ resources/views/users/_coupon_codes.blade.php
@@ -52,9 +52,8 @@
 <div>
 <nav>
   <div class="nav nav-tabs" id="nav-tab" role="tablist">
-    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-
+    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">已激活卡券</a>
+    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">过期卡券</a>
   </div>
 </nav>
 <div class="tab-content" id="nav-tabContent">
