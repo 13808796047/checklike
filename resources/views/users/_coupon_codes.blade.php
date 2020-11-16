277d61445d168b675953d69e9778ae6a5898c633
shahao1025 <361719196@qq.com>
2020-11-16 10:19:30 +0800

    test

--- resources/views/users/_coupon_codes.blade.php
+++ resources/views/users/_coupon_codes.blade.php
@@ -50,7 +50,15 @@
   }
 </style>
 <div>
-
+<nav>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+  </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
   <div style="display: flex;flex-wrap: wrap;">
     @foreach($coupon_codes as $item)
         @if ($item->is_enable == true)
@@ -79,35 +87,12 @@
       </div>
     @endforeach
   </div>
-  <div class="usertitle">过期卡券</div>
-  <div style="display: flex;flex-wrap: wrap;">
-    @foreach($coupon_codes as $item)
-        @if ($item->is_enable == false)
-          @continue
-        @endif
-      <div class="gqcard">
-        <div style="text-align: center;padding-top: 23px;font-size: 29px;font-weight: bold;">
-          @if($item->type =="fixed")
-            ￥{{$item->value}}
-          @else
-            {{$item->value}}折
-          @endif
-        </div>
-        <div style="text-align:center;font-size:11px;">
-          @if($item->type =="vip")
-            VIP专属
-          @else
-            满{{$item->min_amount}}元可用，限一次
-          @endif
-        </div>
-        <div class="cardpline">
-          <p>适用系统：{{$item->category? $item->category->name:"不限"}}</p>
-          <p></p>
-          <p>卡券编号：{{$item->code}}</p>
-        </div>
-      </div>
-    @endforeach
   </div>
+  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+</div>
+
+
 </div>
 <script>
 let fsdafasdf=  {!!$coupon_codes!!}
