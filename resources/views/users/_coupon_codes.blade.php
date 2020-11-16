47b2556bc00ef7d1a2bcd1a82932b169ce11539e
shahao1025 <361719196@qq.com>
2020-11-16 10:16:15 +0800

    test

--- resources/views/users/_coupon_codes.blade.php
+++ resources/views/users/_coupon_codes.blade.php
@@ -50,16 +50,9 @@
   }
 </style>
 <div>
-<nav>
-  <div class="nav nav-tabs" id="nav-tab" role="tablist">
-    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-  </div>
-</nav>
-<div class="tab-content" id="nav-tabContent">
-  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-    <div>
-        @foreach($coupon_codes as $item)
+
+  <div style="display: flex;flex-wrap: wrap;">
+    @foreach($coupon_codes as $item)
         @if ($item->is_enable == true)
           @continue
         @endif
@@ -84,13 +77,37 @@
           <p>卡券编号：{{$item->code}}</p>
         </div>
       </div>
-    </div>
     @endforeach
   </div>
-
-  </div>
-  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
-</div>
+  <div class="usertitle">过期卡券</div>
+  <div style="display: flex;flex-wrap: wrap;">
+    @foreach($coupon_codes as $item)
+        @if ($item->is_enable == false)
+          @continue
+        @endif
+      <div class="gqcard">
+        <div style="text-align: center;padding-top: 23px;font-size: 29px;font-weight: bold;">
+          @if($item->type =="fixed")
+            ￥{{$item->value}}
+          @else
+            {{$item->value}}折
+          @endif
+        </div>
+        <div style="text-align:center;font-size:11px;">
+          @if($item->type =="vip")
+            VIP专属
+          @else
+            满{{$item->min_amount}}元可用，限一次
+          @endif
+        </div>
+        <div class="cardpline">
+          <p>适用系统：{{$item->category? $item->category->name:"不限"}}</p>
+          <p></p>
+          <p>卡券编号：{{$item->code}}</p>
+        </div>
+      </div>
+    @endforeach
+  </div>
 </div>
 <script>
 let fsdafasdf=  {!!$coupon_codes!!}
