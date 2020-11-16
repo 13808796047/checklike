9377663b7525ba1e1869c754391a1c8523ce3adf
shahao1025 <361719196@qq.com>
2020-11-16 10:08:45 +0800

    test

--- resources/views/users/_coupon_codes.blade.php
+++ resources/views/users/_coupon_codes.blade.php
@@ -51,9 +51,18 @@
 </style>
 <div>
 <ul class="nav nav-tabs">
-  <li role="presentation" class="active"><a href="#">Home</a></li>
-  <li role="presentation"><a href="#">Profile</a></li>
-  <li role="presentation"><a href="#">Messages</a></li>
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
 </ul>
 </div>
 <script>
