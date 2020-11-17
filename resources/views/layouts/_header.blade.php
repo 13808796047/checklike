9b653eee44be96b2e538a7d89536f0dec60b0a4f
shahao1025 <361719196@qq.com>
2020-11-17 17:01:52 +0800

    test

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,17 +17,15 @@
             @guest
             <li><a class="nav-link" href="javascript:;" data-toggle="modal" data-target="#staticBackdrop">免费查重</a></li>
             @else
-            <li id="headerfree"><a href="/categories/4" class="smooth-scroll">免费查重</a></li>
-            <div class="btn-group" id="headerfree1" style="display:none;">
-               <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Action
+            <!-- <li id="headerfree"><a href="/categories/4" class="smooth-scroll">免费查重</a></li> -->
+            <div class="dropdown" id="headerfree1">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
   </button>
-  <div class="dropdown-menu">
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
     <a class="dropdown-item" href="#">Action</a>
     <a class="dropdown-item" href="#">Another action</a>
     <a class="dropdown-item" href="#">Something else here</a>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="#">Separated link</a>
   </div>
 </div>
             @endguest
