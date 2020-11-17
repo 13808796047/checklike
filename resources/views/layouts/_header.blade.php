adb30bdb968d0d461e32240a330ae303d6e2d7c3
shahao1025 <361719196@qq.com>
2020-11-17 17:09:06 +0800

    test

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,17 +17,7 @@
             @guest
             <li><a class="nav-link" href="javascript:;" data-toggle="modal" data-target="#staticBackdrop">免费查重</a></li>
             @else
-            <!-- <li id="headerfree"><a href="/categories/4" class="smooth-scroll">免费查重</a></li> -->
-            <div class="dropdown" id="headerfree1">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
+            <li id="headerfree"><a href="/categories/4" class="smooth-scroll">免费查重</a></li>
             @endguest
             @guest
             <li><a class="nav-link" href="javascript:;" data-toggle="modal" data-target="#staticBackdrop">自动降重</a></li>
