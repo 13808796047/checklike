c9536754164cc294d3b70e4976077606cef70e6c
shahao1025 <361719196@qq.com>
2020-11-17 17:26:34 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@
 
 @yield('scripts')
 <script !src="">
-  console.log($request->route()->getAction();)
+  console.log($request->route()->getAction())
   //退出登录
   toastr.options = {
     positionClass: "toast-center-center",
