44181ed478b1c98a77adc366e91fcb6effc5942f
shahao1025 <361719196@qq.com>
2020-11-17 14:42:53 +0800

    test

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -135,9 +135,9 @@
 				<a type="button" id="btn-wechat" style="height:33px;margin:13px auto;display:none;" href="javascript:;"
 						class="btn btn-primary btn-sm sbtn">提交</a>
 	</div>
-  <div id="iszero" style="displaynone;">
-          <p>本次订单免费检测，限1000字内，每日一次。</p>
-          <p>提交</p>
+      <div id="iszero" style="display:none;">
+          <p style="font-size: 13px;color:gray;">本次订单免费检测，限1000字内，每日一次。</p>
+          <p style="color:#fff;background: red;width: 10%;margin: 0 auto;" id="freefee">提交</p>
        </div>
        </div>
 
@@ -195,8 +195,24 @@
       //  }).catch(err=>{
       //    console.log(err,"错误")
       //  })
+       let curPrice = =  {{$order->price}};
+       if(curPrice==0){
+         $("#iszero").css("display","block")
+         $("#isfeizero").css("display","none")
+       }else{
+        $("#iszero").css("display","none")
+         $("#isfeizero").css("display","black")
+       }
+
 
        let currentId = {!!$order->id!!}
+
+       $("#freefee").click(()=>{
+          axios.get(`/payments/${currentId}/free_pay`).then(res=>{
+              location.href = "/orders"
+          })
+       })
+
        axios.get(`/orders/${currentId}/coupon_codes`).then(res=>{
         console.log(res,"fsafs")
         couponArr=res.data.data;
