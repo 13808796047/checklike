c29656180d8eb5c0afb151dd22d41430bbcdfe26
shahao1025 <361719196@qq.com>
2020-11-18 11:44:21 +0800

    test

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -54,29 +54,23 @@
 
       <div class="col-span-5 p-4" style="box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);background:#fff;">
          <!-- 新添加 -->
-         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  <li class="nav-item" role="presentation">
-    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">初稿检测</a>
-  </li>
-  <li class="nav-item" role="presentation">
-    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">维普查重</a>
-  </li>
-  <li class="nav-item" role="presentation">
-    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">万方查重</a>
-  </li>
-  <li class="nav-item" role="presentation">
-    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">知网查重</a>
-  </li>
-  <li class="nav-item" role="presentation">
-    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">PaperPass</a>
-  </li>
-
-</ul>
-<div class="tab-content" id="pills-tabContent">
-  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
-  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
-</div>
+         <ul class="nav nav-pills">
+           <li class="nav-item">
+           <a class="nav-link active" href="/categories/1">初稿查重</a>
+           </li>
+           <li class="nav-item">
+            <a class="nav-link" href="/categories/2">维普查重</a>
+           </li>
+           <li class="nav-item">
+            <a class="nav-link" href="/categories/3">万方查重</a>
+           </li>
+           <li class="nav-item">
+           <a class="nav-link" href="/categories/4">知网查重</a>
+           </li>
+           <li class="nav-item">
+           <a class="nav-link" href="/categories/5">PaperPass</a>
+           </li>
+          </ul>
 
 
       <!-- 添加结束 -->
