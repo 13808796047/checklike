e9b633eb9bedb0ac287bb2d30412261379db0c71
shahao1025 <361719196@qq.com>
2020-11-18 14:57:59 +0800

    test

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -31,9 +31,9 @@
             @endguest
             @guest
 						<li class="menu-btn" style="padding-right:0"><a class="nav-link" href="javascript:;" data-toggle="modal"
-            data-target="#staticBackdrop" >登录/注册</a></li>
-            <li class="menu-btn" style="padding-left:0;display:none;"><a class="nav-link" href="javascript:;"
-            id="RegisterDialogBtn"></a></li>
+            data-target="#staticBackdrop" >登录</a></li>
+            <li class="menu-btn" style="padding-left:0;"><a class="nav-link" href="javascript:;"
+            id="RegisterDialogBtn">注册</a></li>
             @else
             <li class="menu-btn" style="padding-right:0"><a class="logout" href="javascript:;">退出</a></li>
             <li class="menu-btn" style="padding-left:0;margin-left:2px;"><a href="/users/{{Auth::user()->id}}">个人中心</a></li>
