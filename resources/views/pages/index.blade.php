99dcb8fbca3901360bf88b24ac0e0772fd9566fd
shahao1025 <361719196@qq.com>
2020-11-21 17:21:16 +0800

    test

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -89,8 +89,8 @@
 									微信扫一扫 享免费检测
 								</div>
 								<div style="margin: 5px 0;">
-                <div class="d-flex justify-content-center">
-                    <div class="spinner-border" role="status">
+                <div class="d-flex justify-content-center" style="width:200px;height:200px;position:relative;margin:0 auto;">
+                    <div class="spinner-border" role="status" style="position:absolute;top:95px;">
                                 <span class="sr-only">Loading...</span>
                 </div>
                 </div>
