63d3cfcf749f36d44bc71e1501c968b331d531cf
shahao1025 <361719196@qq.com>
2020-11-23 14:39:10 +0800

    test

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -84,7 +84,7 @@
 					</div>
 					<ul class="newul" style="flex:1;width:72%;">
             <div style="display:flex;justify-content:space-between;width:100%;">
-            <div style="display:flex;align-items: center;">
+            <div style="display:flex;align-items: center;margin-left:13px;">
 						      <li><a href="javascript:void(0)" onclick="window.location.href='/'" class="smooth-scroll">网站首页</a></li>
                   <li><a href="/categories/1" class="smooth-scroll">论文查重</a></li>
                   <li><a href="/categories/4" class="smooth-scroll">免费查重</a></li>
