1a47ceb44458f4c8cbadd0e07a965de770655861
shahao1025 <361719196@qq.com>
2020-11-23 15:43:44 +0800

    test

--- resources/views/ai_rewrite/index.blade.php
+++ resources/views/ai_rewrite/index.blade.php
@@ -186,7 +186,7 @@
       </div>
     <div class="col-span-3"  id="jcright">
       <div>
-      <div style="background:#54B538;color: #fff;padding-left: 20px;font-size: 15px;height: 44px;line-height: 44px;">在线客服</div>
+      <div style="background:#54B538;color: #fff;padding-left: 20px;font-size: 15px;height: 44px;line-height: 44px;">系统客服</div>
       <div style="border-bottom: 1px solid #c1bebd;box-shadow: 0px 0px 5px #c1bebd;padding: 15px;background: #FFFFFF;">
           <img src="https://www.checklike.com/images/qrcode/sz-work.png" style="width:171px;height:171px;display:block;margin:0 auto;">
           <p style="text-align:center;margin-top:7px;font-size:13px;">微信扫一扫，与客服在线沟通</p>
