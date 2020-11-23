632ce662e5adb400caf5b85807db4acadbd0f472
shahao1025 <361719196@qq.com>
2020-11-23 16:21:00 +0800

    test

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -95,7 +95,15 @@
 
         <span class="p-2">共{{$orders->total()}}条</span>
         <nav aria-label="Page navigation " id="nav_navigation">
-
+          <ul class="pagination ">
+            <li class="page-item"><a class="page-link" href="{{$orders->previousPageUrl()	}}">上一页</a></li>
+            @for($i=1;$i<=$orders->lastPage();$i++)
+              <li class="page-item {{ $i==$orders->currentPage()?'active':'' }}"><a class="page-link"
+                                                                                    href="{{$orders->url($i)}}">{{$i}}</a>
+              </li>
+            @endfor
+            <li class="page-item"><a class="page-link" href="{{$orders->nextPageUrl()}}">下一页</a></li>
+          </ul>
         </nav>
       </div>
     </div>
@@ -106,106 +114,6 @@
 @section('scripts')
   <script>
     $(function () {
-      function getPagination(totalPage, currentPage){
-
- var paginationInfo = "<ul class='pagination' >";
- if (currentPage == 1) {
-     paginationInfo += "<li class='disabled'><a href='javascript:void(0);' onclick='refreshPages("+totalPage+" , "+(currentPage-1) + ");searchJob(" + (currentPage-1) + ")'"+">«</a></li>";
- }else {
-     //前一页
-     paginationInfo += "<li><a href='javascript:void(0);' onclick='refreshPages("+totalPage+" , "+(currentPage-1) + ");searchJob(" + (currentPage-1) + ")'"+">«</a></li>";
-
- }
-
- if(totalPage<=10){
-     //totalPage<=10
-     for(var i=1; i<=totalPage; i++){
-         if (i == currentPage) {
-             paginationInfo += "<li class='active'> <a href='javascript:void(0);' onclick='refreshPages(" + totalPage + " , " + i + " );searchJob(" + i + ")'>" + i + " </a></li>";
-         }else {
-             paginationInfo += "<li><a href='javascript:void(0);' onclick='refreshPages(" + totalPage + " , " + i + " );searchJob(" + i + ")'>" + i + " </a></li>";
-         }
-     }
- }
- else{
-     //totalPage > 10
-     if(currentPage<=3){
-         for(var i=1; i<=currentPage+2; i++){
-             //页码1、2
-             if (i == currentPage) {
-                 paginationInfo += "<li class='active'> <a href='javascript:void(0);' onclick='refreshPages(" + totalPage + " , " + i + ");searchJob(" + i + ")'>" + i + "</a></li>";
-             } else {
-                 paginationInfo += "<li > <a href='javascript:void(0);' onclick='refreshPages("+ totalPage + " , " + i + ");searchJob(" + i + ")'>" + i + "</a></li>";
-             }
-         }
-         paginationInfo += "<li><a href='javascript:void(0);'>...</a></li>";
-         //最后一页的页码
-         paginationInfo += "<li><a href='javascript:void(0);' onclick='refreshPages(" + totalPage + " , " + totalPage + ");searchJob(" + totalPage + ")'>" + totalPage + "</a></li>";
-     }else if(currentPage<=totalPage-5){
-         //totalPage > 10   currentPage > 3 currentPage<=totalPage-5，  页码在中间部分
-         //页码为1的代码
-         paginationInfo += "<li><a href='javascript:void(0);' onclick='refreshPages("+totalPage+" , "+ 1 +");searchJob(1)'>" + 1 + "</a></li>";
-
-         //页码1后面的省略号
-         paginationInfo += "<li><a href='javascript:void(0);'>...</a></li>";
-
-         //中间部分代码
-         for(var i=currentPage-1; i<=currentPage+2; i++){
-             if (i == currentPage) {
-                 paginationInfo += "<li class='active'> <a href='javascript:void(0);' onclick='refreshPages(" + totalPage + " , " + i + ");searchJob(" + i + ")'>" + i + "</a></li>";
-             } else {
-                 paginationInfo += "<li> <a href='javascript:void(0);' onclick='refreshPages(" + totalPage + " , " + i + ");searchJob(" + i + ")'>" + i + "</a></li>";
-             }
-         }
-         //后面的省略号
-         paginationInfo += "<li><a href='javascript:void(0);'>...</a></li>";
-         //最后一个页码
-         paginationInfo += "<li><a href='javascript:void(0);' onclick='refreshPages("+totalPage+" , "+totalPage+");searchJob(" + totalPage + ")'>"+totalPage+"</a></li>";
-     }else{
-         //totalPage > 10  并且currentPage > totalPage-5 显示后面的页码
-
-         //页码1
-         paginationInfo += "<li><a href='javascript:void(0);' onclick='refreshPages("+totalPage+" , "+1+");searchJob(1)'>"+1+"</a></li>";
-         //省略号
-         paginationInfo += "<li><a href='javascript:void(0);'>...</a></li>";
-         //最后几位页码
-         for(var i=currentPage-1; i<=totalPage; i++){
-             if (i == currentPage) {
-                 paginationInfo += "<li class='active'> <a href='javascript:void(0);' onclick='refreshPages("+totalPage+" , "+i+");searchJob(" + i + "'>"+i+"</a></li>";
-             }else {
-                 paginationInfo += "<li> <a href='javascript:void(0);' onclick='refreshPages("+totalPage+" , "+i+");searchJob(" + i + ")'>"+i+"</a></li>";
-             }
-         }
-     }
- }
-
- //下一页
- if (currentPage == totalPage) {
-     paginationInfo += "<li class='disabled'> <a href='javascript:void(0);' onclick='refreshPages(" + totalPage + " , " + (currentPage + 1) + ");searchJob(" + (currentPage + 1) + ")'" + ">»</a></li>";
- } else {
-     paginationInfo += "<li><a href='javascript:void(0);' onclick='refreshPages("+totalPage+" , "+(currentPage+1)+");searchJob(" + (currentPage+1) + ")'"+">»</a></li>";
- }
- paginationInfo += "</ul>";
- //返回结果
- return paginationInfo;
-}
-
-function refreshPages(totalPage, currentPage) {
-
- //安全判断
- if (currentPage < 1 ) {
-     currentPage = 1;
- }
- if (currentPage > totalPage) {
-     currentPage = totalPage;
- }
- var paginationInfo = getPagination(totalPage, currentPage);
- //用id选择器写入页码部分代码（根据需求修改）
- $("#nav_navigation").html(paginationInfo);
-
-}
-
-refreshPages(25,1)
       setTimeout(() => {
         window.location.reload();
       }, 120000);
