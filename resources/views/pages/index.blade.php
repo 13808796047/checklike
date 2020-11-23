d1851846f2cc01b9da71a0e0eb5dfe3252c8e544
shahao1025 <361719196@qq.com>
2020-11-23 10:33:03 +0800

    test

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -231,7 +231,7 @@
   <div id="header1">
      <nav id="navigation" class="navbar scrollspy">
 				<div class="container">
-					<div class="navbar-brand" style="width:316px;">
+					<div class="navbar-brand">
 						<a href="javascript:void(0)" onclick="window.location.href='/'"><img src= "{{ asset('asset/images/checklike.png') }}" alt=""></a>
 					</div>
 					<ul class="nav navbar-nav" style="flex:1">
