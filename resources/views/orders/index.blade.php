e784e4f640b8452464814b2b47de7934f615ffd2
shahao1025 <361719196@qq.com>
2020-11-23 17:44:54 +0800

    test

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -189,7 +189,7 @@
         nowPage: 1, // 当前页码
         pageNum: 23, // 总页码
         buttonNum: 5, //要展示的页码数量
-        canJump: 1,// 是否能跳转。0=不显示（默认），1=显示
+        canJump: 0,// 是否能跳转。0=不显示（默认），1=显示
         showOne: 1,//只有一页时，是否显示。0=不显示,1=显示（默认）
         callback: function (num) { //回调函数
             console.log(num);
