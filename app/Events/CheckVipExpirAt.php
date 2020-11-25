d0053f03b9a242c2392691ac65aca037ba64b3ef
Summer <csy13808796047@gmail.com>
2020-11-25 09:49:35 +0800

    test

--- app/Events/CheckVipExpirAt.php
+++ app/Events/CheckVipExpirAt.php
@@ -16,18 +16,10 @@ class CheckVipExpirAt
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    protected $user;
+    public $user;
 
-    public function __construct($user)
+    public function __construct(User $user)
     {
-        Log::info('dsa', [$user]);
         $this->user = $user;
-    }
-
-    public function getUser()
-    {
-        Log::info('dsa', [$this->user]);
-
-        return $this->user;
     }
 }
