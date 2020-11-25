3a718fd0946faab172962b8de8aa9713393ee61d
Summer <csy13808796047@gmail.com>
2020-11-25 09:34:29 +0800

    test

--- app/Events/CheckVipExpirAt.php
+++ app/Events/CheckVipExpirAt.php
@@ -10,6 +10,7 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class CheckVipExpirAt
 {
@@ -19,6 +20,7 @@ class CheckVipExpirAt
 
     public function __construct(User $user)
     {
+        Log::info('dsa', [$user]);
         $this->user = $user;
     }
 
