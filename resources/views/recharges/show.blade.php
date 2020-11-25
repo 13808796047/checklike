9faf2a9f65f257ae61a9c36a98c3716e0b99e9e1
shahao1025 <361719196@qq.com>
2020-11-25 14:18:36 +0800

    test

--- resources/views/recharges/show.blade.php
+++ resources/views/recharges/show.blade.php
@@ -47,6 +47,27 @@
   </style>
 @stop
 @section('content')
+
+<div class="modal fade" id="wxcodeTcDialog" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="wxcodeTcDialogLabel" aria-hidden="true" data-backdrop="static">
+  <div class="modal-dialog modal-dialog-centered" style="width:333px;">
+    <div class="modal-content">
+      <div class="modal-body">
+         <div style="padding:10px 17px;">
+            <p style="text-align:center;font-size:16px;color:#808080;">打开微信使用扫一扫完成付款</p>
+            <div>
+              <img src="" alt="" srcset="" id="codeurl" style="width:210px;height:210px;display:block;margin:0 auto;">
+            </div>
+            <div style="display:flex;justify-content: center;margin-top:5px;">
+              <button type="button" class="btn btn-secondary" id="closewxCodeDialog">关闭</button>
+              <div style="margin:0 10px;"></div>
+              <button type="button" class="btn btn-primary" id="completewxCodeDialog">已完成付款</button>
+            </div>
+         </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 <!-- 购买降重字数模态框 -->
 <div class="modal fade bd-example-modal-sm" id="wxModal" tabindex="-1" role="dialog"
     aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -139,38 +160,6 @@
 				<a type="button" id="btn-wechat" style="height:33px; margin-left:20px; margin-left:320px;display: none" href="javascript:;"
 						class="btn btn-primary btn-sm sbtn">提交</a>
 	     </div>
-
-						<!-- <table class="mylist" style="line-height: 30px">
-            <tr>
-
-							<tr bgcolor="#D0EAFF">
-								<td colspan="2" align="center">
-									<b>
-										<font color="#BF2020">支付检测费用（请选择以下任意一种方式支付）</font>
-									</b>
-								</td>
-							</tr>
-
-							<tr>
-								<td class="td">
-									在线支付<font color="#FF0004">(推荐)</font>
-								</td>
-								<td class="td">
-									<div style="display:flex;align-items: center">
-										<input type="radio" name="paytype" value="alipay" checked="checked" />
-										<img src="{{asset('asset/images/alipay.png')}}" style="margin-left:17px;" />
-									</div>
-									&nbsp;&nbsp;
-									<div style="display:flex;align-items: center">
-										<input type="radio" name="paytype" value="wxpay" />
-										<img src="{{asset('asset/images/wxpay.png')}}" style="width:99px;margin-left:17px;" />
-									</div>
-									<div class="tips">
-										直接使用支付宝或者微信支付，即时返回支付结果，方便快捷，推荐使用。
-									</div>
-								</td>
-							</tr>
-						</table> -->
 </div>
     <div class="col-span-3">
       <div>
