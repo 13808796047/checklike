cefdff803c91d34a4c21fb5c1646f335d3511531
Summer <csy13808796047@gmail.com>
2020-11-26 11:35:50 +0800

    PC-微信公众号登录

--- app/Http/Controllers/OfficialAccountController.php
+++ app/Http/Controllers/OfficialAccountController.php
@@ -107,7 +107,9 @@ class OfficialAccountController extends Controller
         $wxUser = $this->app->user->get($openId);
         if($user = User::where('weixin_openid', $this->openid)->first()) {
             Log::info('用户前', [$user]);
-            $user->weixin_unionid = $wxUser['weixin_unionid'];
+            $user->update([
+                'weixin_unionid' => $wxUser['unionid'],
+            ]);
             Log::info('用户后', [$user]);
             // 标记前端可登录
             $this->markTheLogin($event, $user->id);
