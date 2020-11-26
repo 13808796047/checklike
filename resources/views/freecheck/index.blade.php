7309f003155caf98e3630147b4430faf02a7c77c
shahao1025 <361719196@qq.com>
2020-11-26 10:26:26 +0800

    test

--- resources/views/freecheck/index.blade.php
+++ resources/views/freecheck/index.blade.php
@@ -71,7 +71,7 @@
     }
     .freecheckleft p{
       text-indent:2em;
-      line-height:2;
+      line-height:2.3;
       font-size:15px;
     }
   </style>
@@ -278,7 +278,7 @@
 	<div class="col-span-9 p-4" style="box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);background: #fff;">
   <div style="padding:30px 50px;" class="freecheckleft">
     <div style="text-align: center;font-size: 23px;font-weight: bold;">真金不怕火炼，CheckLike提供免费查重服务</div>
-    <div style="font-size:13px;color:#A9A9A9;text-align:center;margin:5px 0;">活动时间：2020-10-01 至2021-07-31</div>
+    <div style="font-size:13px;color:#A9A9A9;text-align:center;margin:15px 0;">活动时间：2020-10-01 至2021-07-31</div>
     <p>CheckLike论文相似度检测系统采用国际领先的海量论文动态语义跨域识别加指纹比对技术，通过运用最新的云检测服务部署使其能够快捷、稳定、准确地检测到文章中存在的抄袭和不当引用现象，实现了对学术不端行为的检测服务。</p >
     <p>通过系统检测可快速准确地检测出论文中不当引用、过度引用甚至是抄袭、伪造、篡改等学术不端行为，可自动生成检测报告，并支持PDF、网页等浏览格式。详细的检测报告通过不同颜色标注相似片段、引用片段、专业用语，形象直观地显示相似内容比对、相似文献汇总、引用片段出处、总相似比、引用率、复写率和自写率等重要指标，为教育机构、科研单位、各级论文评审单位和发表单位提供了论文原创性和新颖性评价的重要依据。经过不断发展和努力，已经在众多行业和部门得到了广泛使用，受到了用户的高度评价。</p>
     <p>为了让更多用户体验到CheckLike的检测效果，我们推出免费查重活动：</p>
@@ -288,11 +288,11 @@
 
     @guest
     <div style="text-align:center;vertical-align:middle;">
-    <a class="btn btn-primary btn-sm sbtn" href="javascript:;" data-toggle="modal" data-target="#staticBackdrop" type="button" style="color:#fff;margin-top:50px;">立即去提交论文</a>
+    <a class="btn btn-primary btn-sm sbtn" href="javascript:;" data-toggle="modal" data-target="#staticBackdrop" type="button" style="color:#fff;margin-top:30px;padding:10px 15px;font-size:18px;background:#0b81fb;">立即去提交论文</a>
     </div>
     @else
     <div style="text-align:center;vertical-align:middle;">
-    <a href="/categories/1" type="button" style="color:#fff;margin-top:50px;" class="btn btn-primary btn-sm sbtn">立即去提交论文</a>
+    <a href="/categories/1" type="button" style="color:#fff;margin-top:30px;padding:10px 15px;font-size:18px;background:#0b81fb;" class="btn btn-primary btn-sm sbtn">立即去提交论文</a>
     </div>
     @endguest
 
@@ -310,11 +310,13 @@
       </div>
       </div>
       <div style="background:#fff;box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);margin-top:13px;" class="p-4">
-        <b>1、论文上传之后安全吗？</b>
+        <b>1、检测结果是否准确？</b>
+        <p>CheckLike是通用检测系统，能够检测出大部分相似文献内容，适合初稿。定稿建议使用与学校或评审机构一致的系统检测一遍，这样比较准确。</p>
+        <b>2、检测需要多少时间？</b>
+        <p>正常情况，检测需要10分钟左右，高峰期可能会延迟。如果长时间未出结果请联系客服解决。</p>
+        <b>3、论文上传之后安全吗？</b>
         <p>本系统有明确的条文规定并遵守严格的论文保密规定，对所有用户提交的送检文档仅做检测分析，绝不保留全文，承诺对用户送检的文档不做任何形式的收录和泄露。</p>
-        <b>2、提交以后能不能退款？</b>
-        <p>系统一旦提交，开始检测后，即产生消费，无法退款，请谅解！</p>
-        <b>3、检测内容范围？</b>
+        <b>4、检测内容范围？</b>
         <p>系统主要检测论文正文，封面、目录、致谢以及个人信息可以删除后再上传。</p>
       </div>
     </div>
