e021a4cab5271bf3d1e6293091910dc192295faf
Summer <csy13808796047@gmail.com>
2020-11-28 10:24:31 +0800

    PC-微信公众号登录

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -34,9 +34,9 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewHorizon', function($user) {
-            return in_array($user->phone, [
-                '13808796047'
-            ]);
+//            return in_array($user->phone, [
+//                '13808796047'
+//            ]);
         });
     }
 }
