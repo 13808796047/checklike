ff5676d39ff940ef3565d27ade20c6bc4e39b066
shahao1025 <361719196@qq.com>
2020-11-30 16:42:48 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,6 @@
   <!-- <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
   <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css"/> -->
 
-
   <link href="{{asset('asset/css/styles.css')}}" rel="stylesheet"/>
   <link rel="stylesheet" href="{{asset('asset/css/jquery-confirm.css')}}">
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -555,7 +555,6 @@
   <script type="text/javascript" src=" {{ asset('asset/js/easing.js') }}"></script> -->
   {{--  <script src="{{ asset('asset/js/responsiveslides.min.js') }}"></script>--}}
   <!-- JavaScripts -->
-
 <!-- <script type="text/javascript" src="{{asset('asset/newjs/jquery-1.11.3.min.js')}}"></script> -->
 <script type="text/javascript" src="{{asset('asset/newjs/jquery-migrate-1.2.1.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('asset/js/jquery-confirm.js')}}" ></script>
