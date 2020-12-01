4e63bfdf1da10bbc8331faa768cdddceb33418e1
shahao1025 <361719196@qq.com>
2020-12-01 15:15:12 +0800

    test

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -189,7 +189,7 @@
 <script type="text/javascript" src="{{ asset('asset/js/pagination.js') }}"></script>
   <script>
     $(function () {
-      console.log({{Auth::user()->phone}},"fsdaf")
+      console.log({{Auth::user()}})
       var last_page={{$orders->lastPage()}}
       var current_page = {{$orders->currentPage()}}
       let a =new Paging('page', {
