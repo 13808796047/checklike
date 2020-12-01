218a3a75073eca307a228cde5522706c8e782884
shahao1025 <361719196@qq.com>
2020-12-01 17:40:27 +0800

    test

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -213,7 +213,6 @@
     boxWidth: '300px',
 
     content:` <img src="https://www.checklike.com/images/qrcode/sz-work.png" style="width:171px;height:171px;display:block;margin:0 auto;">
-          <div style="margin:0 auto;">关闭</div>
     `,
 });
 
