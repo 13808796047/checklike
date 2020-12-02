f7804eb3db6a62436903a4feda1bd60dc40ae6a5
Summer <csy13808796047@gmail.com>
2020-12-02 15:59:14 +0800

    PC-微信公众号登录

--- app/Admin/Controllers/OrderController.php
+++ app/Admin/Controllers/OrderController.php
@@ -41,6 +41,7 @@ class OrderController extends AdminController
 
         return Grid::make(Order::with(['category', 'user']), function(Grid $grid) {
             $grid->id->sortable()->display(function($id) {
+                dd($this);
 //                return "<a href='orders/{$this->id}/download_report'>$id</a>";
                 return Storage::disk('downloads')->download($this->report_path, $this->writer . '-' . $this->title . '.zip');
             });
