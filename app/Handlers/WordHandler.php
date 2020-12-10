05ab3a49ef0d302e807611e2b432073a506700dd
Summer <csy13808796047@gmail.com>
2020-12-10 10:52:33 +0800

    PC-微信公众号登录

--- app/Handlers/WordHandler.php
+++ app/Handlers/WordHandler.php
@@ -12,16 +12,14 @@ class WordHandler
     public function save($content, $folder, $file_prefix)
     {
         $phpWord = new \PhpOffice\PhpWord\PhpWord();
-        dd($content);
-        $template = $phpWord->loadTemplate(public_path('template/template.docx'));
-        $template->setValue('content', $content);
-//        $section = $phpWord->addSection();
-//        $contentFormat = str_replace("\r\n", "<w:br />", $content);
-////        $contentFormat = htmlspecialchars($content);
-//        $section->addText($contentFormat);
+        $section = $phpWord->addSection();
+        $contentFormat = str_replace("\r\n", "<w:br />", $content);
+        dd($contentFormat);
+//        $contentFormat = htmlspecialchars($content);
+        $section->addText($contentFormat);
         // 保存文件
         //生成的文檔爲Word2007
-//        $writer = IOFactory::createWriter($phpWord, 'Word2007');
+        $writer = IOFactory::createWriter($phpWord, 'Word2007');
         $folder_name = "uploads/$folder/" . date('Ym/d', time());
         $upload_path = public_path() . '/' . $folder_name;
         $filename = $file_prefix . '_' . time() . '_' . \Str::random(10) . '.docx';
@@ -31,7 +29,7 @@ class WordHandler
                 chmod($upload_path, 0777);
             }
             chmod($upload_path, 0777);
-            $template->save($upload_path . '/' . $filename);
+            $writer->save($upload_path . '/' . $filename);
             return [
                 'path' => config('app.url') . "/$folder_name/$filename"
             ];
