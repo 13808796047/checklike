1d361dc7c3ca3edf53ceea637df2ee7a5c98b397
Summer <csy13808796047@gmail.com>
2020-12-10 10:58:45 +0800

    PC-微信公众号登录

--- app/Handlers/WordHandler.php
+++ app/Handlers/WordHandler.php
@@ -13,10 +13,10 @@ class WordHandler
     {
         $phpWord = new \PhpOffice\PhpWord\PhpWord();
         $section = $phpWord->addSection();
-        $contentFormat = str_replace("\r\n", "<w:br />", $content);
-        dd($contentFormat);
+//        $contentFormat = str_replace("\r\n", "<w:br />", $content);
+//        dd($contentFormat);
 //        $contentFormat = htmlspecialchars($content);
-        $section->addText($contentFormat);
+        $section->addText($content);
         // 保存文件
         //生成的文檔爲Word2007
         $writer = IOFactory::createWriter($phpWord, 'Word2007');
