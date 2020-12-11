db6c68fe454979de00d8eff3f55515616326ceab
Summer <csy13808796047@gmail.com>
2020-12-11 10:14:06 +0800

    PC-微信公众号登录

--- app/Jobs/OrderPendingMsg.php
+++ app/Jobs/OrderPendingMsg.php
@@ -26,32 +26,33 @@ class OrderPendingMsg implements ShouldQueue
 
     public function handle()
     {
-        if($this->order->status == 0) {
-            $data = [
-                'first' => '您有一个订单尚未完成支付，支付后开始检测',
-                'keyword1' => ['value' => $this->order->title, 'color' => '#173177'],
-                'keyword2' => ['value' => OrderEnum::getStatusName($this->order->status), 'color' => '#173177'],
-                'keyword3' => ['value' => $this->order->created_at->format("Y-m-d H:i:s"), 'color' => '#173177'],
-                'keyword4' => ['value' => $this->order->category->name, 'color' => '#173177'],
-                'keyword5' => ['value' => $this->order->price, 'color' => '#173177'],
-                'remark' => ['value' => '点击去支付', 'color' => '#173177']
-            ];
-            $touser = $this->order->user->weixin_openid;
-            $template_id = config('wechat.official_account.templates.pending.template_id');
-            $appid = config('wechat.official_account.templates.pending.appid');
-            $pagepath = config('wechat.official_account.templates.pending.page_path');
-            if($touser) {
-                app('official_account')->template_message->send([
-                    'touser' => $touser,
-                    'template_id' => $template_id,
+        if($this->order->status != 0) {
+            return;
+        }
+        $data = [
+            'first' => '您有一个订单尚未完成支付，支付后开始检测',
+            'keyword1' => ['value' => $this->order->title, 'color' => '#173177'],
+            'keyword2' => ['value' => OrderEnum::getStatusName($this->order->status), 'color' => '#173177'],
+            'keyword3' => ['value' => $this->order->created_at->format("Y-m-d H:i:s"), 'color' => '#173177'],
+            'keyword4' => ['value' => $this->order->category->name, 'color' => '#173177'],
+            'keyword5' => ['value' => $this->order->price, 'color' => '#173177'],
+            'remark' => ['value' => '点击去支付', 'color' => '#173177']
+        ];
+        $touser = $this->order->user->weixin_openid;
+        $template_id = config('wechat.official_account.templates.pending.template_id');
+        $appid = config('wechat.official_account.templates.pending.appid');
+        $pagepath = config('wechat.official_account.templates.pending.page_path');
+        if($touser) {
+            app('official_account')->template_message->send([
+                'touser' => $touser,
+                'template_id' => $template_id,
 //                'url' => 'https://wap.lianwen.com/bading?openid=' . $this->order->user->weixin_openid,
-                    'miniprogram' => [
-                        'appid' => $appid,
-                        'pagepath' => $pagepath,
-                    ],
-                    'data' => $data,
-                ]);
-            }
+                'miniprogram' => [
+                    'appid' => $appid,
+                    'pagepath' => $pagepath,
+                ],
+                'data' => $data,
+            ]);
         }
 
     }
