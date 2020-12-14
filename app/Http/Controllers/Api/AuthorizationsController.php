891373fde9a74e979f44eab8a38897867edb25f9
Summer <csy13808796047@gmail.com>
2020-12-14 15:49:25 +0800

    PC-微信公众号登录

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -74,7 +74,6 @@ class AuthorizationsController extends Controller
             "client_id" => config('services.baidu_weapp.client_id'),
             "sk" => config('services.baidu_weapp.client_secret')
         ];
-        return $data;
         $ret = $this->curlPost($url, $data);
         if($iv = $request->iv) {
             $encryptData = $request->encryptData;
