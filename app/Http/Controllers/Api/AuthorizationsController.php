94ea3a6fe7106a6570d2d72cc07a3f9a77cf73e5
Summer <csy13808796047@gmail.com>
2020-12-14 13:57:56 +0800

    PC-微信公众号登录

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -148,16 +148,16 @@ class AuthorizationsController extends Controller
 
     public function curlPost($url, $postDataArr)
     {
-        $headerArr = [
-            "Content-type:application/x-www-form-urlencoded"
-        ];
+//        $headerArr = [
+//            "Content-type:application/x-www-form-urlencoded"
+//        ];
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $postDataArr);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArr);
+//        curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArr);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($curl);
         curl_close($curl);
