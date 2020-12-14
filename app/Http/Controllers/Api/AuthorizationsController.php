b055de06424836a73a578058777975dce6e76938
Summer <csy13808796047@gmail.com>
2020-12-14 14:20:57 +0800

    PC-微信公众号登录

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -75,6 +75,8 @@ class AuthorizationsController extends Controller
             "sk" => 'sximbwDoqFRtDbpPdKh8OQeB419y3Djh'
         ];
         $ret = $this->curlPost($url, $data);
+        $decryptedData = $this->decrypt($encryptData, $iv, config('pay.app_id'), $ret['session_key']);
+        return $decryptedData;
         if($iv = $request->iv) {
             $encryptData = $request->encryptData;
             $decryptedData = $this->decrypt($encryptData, $iv, config('pay.app_id'), $ret['session_key']);
