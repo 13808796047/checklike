6d41c1efaa8fd10a3bcfaeb53bcfee9917136e03
Summer <csy13808796047@gmail.com>
2020-12-14 11:50:42 +0800

    PC-微信公众号登录

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -71,7 +71,7 @@ class AuthorizationsController extends Controller
         $url = 'https://spapi.baidu.com/oauth/jscode2sessionkey';
         $data = [
             "code" => $code,
-            'tp_apppid' => '16777148',
+            'tp_appid' => '16777148',
             "client_id" => 'eSVYdwR78OPcxbdKhj0uXDAbdzBEUSQB',
             "sk" => 'sximbwDoqFRtDbpPdKh8OQeB419y3Djh'
         ];
