ae05b9fbfe1108b8d34289115f74ebe1c1443fcb
Summer <csy13808796047@gmail.com>
2020-12-21 13:08:51 +0800

    PC-微信公众号登录

--- app/Jobs/IOSPaidMessage.php
+++ app/Jobs/IOSPaidMessage.php
@@ -51,13 +51,12 @@ class IOSPaidMessage implements ShouldQueue
             ]);
         }
         if($this->order->phone) {
-            $data = [
-                'phone' => $this->order->phone,
-            ];
             try {
                 $result = $easySms->send($this->order->phone, [
                     'template' => config('easysms.gateways.aliyun.templates.ios_paiding'),
-                    'data' => $data
+                    'data' => [
+                        'phone' => $this->order->phone,
+                    ]
                 ]);
             } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
                 $message = $exception->getException('aliyun')->getMessage();
