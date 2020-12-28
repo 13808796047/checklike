e9b07b99f05686e26639fcb739b54c94d98c877a
shahao1025 <361719196@qq.com>
2020-12-28 10:47:55 +0800

    test

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -557,7 +557,7 @@
   <!-- JavaScripts -->
 <!-- <script type="text/javascript" src="{{asset('asset/newjs/jquery-1.11.3.min.js')}}"></script> -->
 <script type="text/javascript" src="{{asset('asset/newjs/jquery-migrate-1.2.1.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('asset/js/jquery-confirm.js')}}" ></script>
+<!-- <script type="text/javascript" src="{{asset('asset/js/jquery-confirm.js')}}" ></script> -->
 <script type="text/javascript" src="{{asset('asset/newjs/script.js')}}"></script>
 <!-- <script type="text/javascript" src="{{asset('asset/newjs/bootstrap.min.js')}}"></script> -->
 <script type="text/javascript" src="{{asset('asset/newjs/jquery.easing.min.js')}}"></script>
