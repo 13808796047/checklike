6c6ce426fe3a1f6bb6be249c2bb248c6519c9c56
shahao1025 <361719196@qq.com>
2020-12-30 11:13:11 +0800

    test

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -155,14 +155,14 @@
               height: auto;width:100px;">
             </div>
         </div>
-        <div style="display:flex;justify-content: center;">
+
           <a type="button" id="bottonsubmit" style="height:33px;width:200px;margin-top:50px;" href="javascript:;"
 						 class="btn btn-primary btn-sm sbtn">提交</a>
-        </div>
-        <div style="display:flex;justify-content: center;">
+
+
         <a type="button" id="btn-wechat" style="height:33px;width:200px;margin-top:50px;display:none;" href="javascript:;"
 						class="btn btn-primary btn-sm sbtn">提交</a>
-        </div>
+
 	     </div>
       </div>
       <div style="margin-left:20px;width:300px;">
