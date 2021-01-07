68dc22043b6bc699408c4f6d86090acfe2283bf4
shahao1025 <361719196@qq.com>
2021-01-07 09:44:23 +0800

    test

---
+++ resources/views/verificationver/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('title','报告验真')
+@section('styles')
+<style>
+
+</style>
+@stop
+@section('content')
+<!-- Modal -->
+@guest
+<div>
+  报告验真
+</div>
+
+
+<!--/.fluid-container-->
+
+@endsection
+@section('scripts')
+  <script>
+      $(function () {
+      })
+  </script>
+@stop
